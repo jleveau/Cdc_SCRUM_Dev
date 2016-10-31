7fca73301a58391b50c23b4d20bd4dda535f7875
tinouS <tinoudi.simohammed@gmail.com>
2016-10-31 19:06:45 +0100

    inscription etape 1

--- web/SCRUMApp/server/routes/users.js
+++ web/SCRUMApp/server/routes/users.js
@@ -6,24 +6,22 @@ var body = require('body-parser');
 var user = require('./../../models/user');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource man');
+router.get('/', function (req, res, next) {
+    res.send('respond with a resource man');
 });
 
 /**
  * route to add a new user
  * first that verify if there is an existing user in DB.
  */
-router.post('/adduser', function(req, res, next) {
-    user.count(req.body.username, req.body.email, function(count){
-       if(count>0){
-           console.log("REQUEST DENIED");
-           return;
-       }else
-       {
-           user.addUser(req.body.username, req.body.email, req.body.password);
-           res.send('respond with a resource tinoudi');
-       }
+router.post('/adduser', function (req, res, next) {
+    user.count(req.body.username, req.body.email, function (count) {
+        if (count > 0) {
+            console.log("REQUEST DENIED");
+        } else {
+            user.addUser(req.body.username, req.body.email, req.body.password);
+            res.send('respond with a resource');
+        }
         //TODO add flush messages
     });
 });
