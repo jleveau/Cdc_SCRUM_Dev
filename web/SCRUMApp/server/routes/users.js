6d5d0657def79ce6e3d1606c402aee4231542ed2
tinouS <tinoudi.simohammed@gmail.com>
2016-10-31 17:05:55 +0100

    updating users.js, user_registartion.jade, package.json

--- web/SCRUMApp/server/routes/users.js
+++ web/SCRUMApp/server/routes/users.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var body = require('body-parser');
+
+
+var user = require('./../../models/user');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  res.send('respond with a resource man');
+});
+
+/**
+ * route to add a new user
+ * first that verify if there is an existing user in DB.
+ */
+router.post('/adduser', function(req, res, next) {
+    user.count(req.body.username, req.body.email, function(count){
+       if(count>0){
+           console.log("REQUEST DENIED");
+           return;
+       }else
+       {
+           user.addUser(req.body.username, req.body.email, req.body.password);
+           res.send('respond with a resource tinoudi');
+       }
+        //TODO add flush messages
+    });
 });
 
 module.exports = router;
