5035542e5d5173d2644ec5bec579d37c725874ff
tinouS <tinoudi.simohammed@gmail.com>
2016-10-31 21:54:05 +0100

    password crypted

--- web/SCRUMApp/models/userShema.js
+++ web/SCRUMApp/models/userShema.js
@@ -1,23 +1,39 @@
 var mongoose = require('mongoose');
-
+var bcrypt = require('bcrypt');
 var Schema = mongoose.Schema;
+var SALT_WORK_FACTOR = 10;
 
 // create a shema
 
 var userShema = new Schema({
-    username: String,
-    mail: String,
+    username: {type: String, unique: true},
+    mail: {type: String, unique: true},
     password: String,
     image: Buffer,
     first_name: String,
     last_name: String,
-   // followed_projects: [{ type : Number, ref: 'projects' }],
+    // followed_projects: [{ type : Number, ref: 'projects' }],
     date_created: Date,
     date_updated: Date
 });
 
+userShema.pre('save', function(next) {
+    var user = this;
+    if (!user.isModified('password')) return next();
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if (err) return next(err);
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) return next(err);
+            user.password = hash;
+            next();
+        });
+    });
+});
+
+
+
 // a model using the shema
-var myusers = mongoose.model('users',userShema);
+var myusers = mongoose.model('users', userShema);
 
 // make this available in node applications
 module.exports = myusers
