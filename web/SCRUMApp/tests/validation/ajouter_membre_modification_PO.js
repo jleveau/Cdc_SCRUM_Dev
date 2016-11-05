7f2ded097c97ddf97f7a27303e3e5d729e92e9ab
lilianamllf <liliana.mllf@gmail.com>
2016-11-05 23:29:58 +0100

    test de validation

---
+++ web/SCRUMApp/tests/validation/ajouter_membre_modification_PO.js
@@ -0,0 +1,35 @@
+require('geckodriver');
+var assert = require("chai").assert;
+require('chromedriver');
+require('mocha');
+
+var selenium = require('selenium-webdriver');
+var test = require('selenium-webdriver/testing');
+
+const timeOut = 15000;
+
+
+test.describe('Testing modification members projet', function() {
+    this.timeout(timeOut);
+
+
+    before(function() {
+
+    });
+
+    after(function() {
+        driver.quit();
+    });
+
+    test.it("ajouter member et changer PO", function() {
+        driver = new selenium.Builder().withCapabilities(selenium.Capabilities.chrome()).build();
+        driver.get("http://localhost:8080/project/581e54710bcffd3721e0c3f9").then(function(){
+            
+			driver.manage().timeouts().implicitlyWait(50000).then(function(){
+                
+				driver.findElement(selenium.By.name('login')).click();
+				
+                });
+            });
+        });
+    });
