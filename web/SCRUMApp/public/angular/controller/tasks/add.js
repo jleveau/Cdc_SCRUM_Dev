0f460664cb6ea5756d1095a7b1c520fd72945d93
Julien Leveau <oursgroumy@gmail.com>
2016-11-08 18:03:20 +0100

    html for Tasks

--- web/SCRUMApp/public/angular/controller/tasks/add.js
+++ web/SCRUMApp/public/angular/controller/tasks/add.js
@@ -2,9 +2,27 @@
  * Created by julien on 07/11/16.
  */
 angular.module('Tasks',[])
-    .controller('TasksAddController', ['$scope', '$location', 'TasksServices',
-        function ($scope, $location, TasksServices) {
+    .controller('TasksAddController', ['$scope', '$location','$mdDialog', 'TasksServices',
+        function ($scope, $location,$mdDialog, TasksServices) {
 
+            $scope.showPrompt = function($event){
+                    var parentEl = angular.element(document.body);
+                $mdDialog.show({
+                    parent: parentEl,
+                    targetEvent: $event,
+                    templateUrl: '/partials/dialog_tasks.jade',
+                    locals: {
+                        items: $scope.items
+                    },
+                    controller: DialogController
+                });
+                function DialogController($scope, $mdDialog, items) {
+                    $scope.items = items;
+                    $scope.closeDialog = function() {
+                        $mdDialog.hide();
+                    }
+                }
 
+            };
 
     }]);
