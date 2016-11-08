c258f6c79dd86a147c709310bf53eeeac1031461
tinouS <tinoudi.simohammed@gmail.com>
2016-11-08 11:04:28 +0100

    resolving merge

--- web/SCRUMApp/tests/validation/inscription_login.js
+++ web/SCRUMApp/tests/validation/inscription_login.js
@@ -10,50 +10,51 @@ const timeOut = 15000;
 
 
 test.describe('Testing inscription et login', function() {
-    this.timeout(timeOut);
+	this.timeout(timeOut);
 
 
-    before(function() {
+	before(function () {
 
-    });
+	});
 
-    after(function() {
-        driver.quit();
-    });
+	after(function () {
+		driver.quit();
+	});
 
-    test.it("create utilisateur et faire login", function() {
-        driver = new selenium.Builder().withCapabilities(selenium.Capabilities.chrome()).build();
-        driver.get("http://localhost:8080/").then(function(){
-            
-			driver.manage().timeouts().implicitlyWait(50000).then(function(){
-                
-				driver.findElement(selenium.By.name('username')).sendKeys("ana").then(function(input) {
-                    
-					driver.findElement(selenium.By.name('mail')).sendKeys("ana@g.com").then(function(input) {
-						
-						driver.findElement(selenium.By.name('password')).sendKeys("ana").then(function(input) {
-							
+	test.it("create utilisateur et faire login", function () {
+		driver = new selenium.Builder().withCapabilities(selenium.Capabilities.chrome()).build();
+		driver.get("http://localhost:8080/").then(function () {
+
+			driver.manage().timeouts().implicitlyWait(50000).then(function () {
+
+				driver.findElement(selenium.By.name('username')).sendKeys("ana").then(function (input) {
+
+					driver.findElement(selenium.By.name('mail')).sendKeys("ana@g.com").then(function (input) {
+
+						driver.findElement(selenium.By.name('password')).sendKeys("ana").then(function (input) {
+
 							driver.findElement(selenium.By.id('button-registration-home')).click();
-							driver.wait(function() {
-								
-								driver.findElement(selenium.By.name('username')).sendKeys("ana").then(function(input) { 
+							driver.wait(function () {
 
-									driver.findElement(selenium.By.name('password')).sendKeys("ana").then(function(input) { 
-										
+								driver.findElement(selenium.By.name('username')).sendKeys("ana").then(function (input) {
+
+									driver.findElement(selenium.By.name('password')).sendKeys("ana").then(function (input) {
+
 										driver.findElement(selenium.By.name('login')).click();
-										driver.wait(function() {
+										driver.wait(function () {
 
-											return driver.getElementsByTagName('h4')[0].innerHTML.then(function(text) {
+											return driver.getElementsByTagName('h4')[0].innerHTML.then(function (text) {
 												return text === 'ana';
 											});
-										}, 1000);
-								 		
+										}, 50000);
+
 									});
-					  	    	});
-							}, 1000);
+								});
+							}, 50000);
 						});
-                    });
-                });
-            });
-        });
-    });
+					});
+				});
+			});
+		});
+	});
+});
