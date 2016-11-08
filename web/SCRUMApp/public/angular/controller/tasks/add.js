b27ac9b667e4f64578be89024bdb04dbf42f7390
Julien Leveau <oursgroumy@gmail.com>
2016-11-08 20:16:01 +0100

    adding dependencies modal to tasks page

--- web/SCRUMApp/public/angular/controller/tasks/add.js
+++ web/SCRUMApp/public/angular/controller/tasks/add.js
@@ -5,12 +5,32 @@ angular.module('Tasks',[])
     .controller('TasksAddController', ['$scope', '$location','$mdDialog', 'TasksServices',
         function ($scope, $location,$mdDialog, TasksServices) {
 
-            $scope.showPrompt = function($event){
+            $scope.showUserStory = function($event){
                     var parentEl = angular.element(document.body);
                 $mdDialog.show({
                     parent: parentEl,
                     targetEvent: $event,
-                    templateUrl: '/partials/dialog_tasks.jade',
+                    templateUrl: '/partials/dialog_tasks_userstories.jade',
+                    locals: {
+                        items: $scope.items
+                    },
+                    controller: DialogController
+                });
+                function DialogController($scope, $mdDialog, items) {
+                    $scope.items = items;
+                    $scope.closeDialog = function() {
+                        $mdDialog.hide();
+                    }
+                }
+
+            };
+
+            $scope.showDependencies = function($event){
+                var parentEl = angular.element(document.body);
+                $mdDialog.show({
+                    parent: parentEl,
+                    targetEvent: $event,
+                    templateUrl: '/partials/dialog_tasks_dependencies.jade',
                     locals: {
                         items: $scope.items
                     },
