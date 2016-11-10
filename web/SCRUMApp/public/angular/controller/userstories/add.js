d6abf41e4d4e80a0d63ef290011a8b512da02e7b
tinouS <tinoudi.simohammed@gmail.com>
2016-11-10 13:40:30 +0100

    add userStory - not finished yet

--- web/SCRUMApp/public/angular/controller/userstories/add.js
+++ web/SCRUMApp/public/angular/controller/userstories/add.js
@@ -1,10 +1,15 @@
-/**
- * Created by julien on 07/11/16.
- */
 angular.module('UserStories',[])
     .controller('UserStoriesAddController', ['$scope', '$location', 'UserStoriesServices',
         function ($scope, $location, UserStoriesServices) {
+     $scope.params = $routeParams;
 
+    if($scope.params.project_id){
+        UserStoriesServices.get($scope.params.project_id).then(function (response) {
+            $scope.userStory = response.data;
+        })
+    }
 
-
+     $scope.createUserStory = function () {
+        console.log($scope.params);
+     };
     }]);
