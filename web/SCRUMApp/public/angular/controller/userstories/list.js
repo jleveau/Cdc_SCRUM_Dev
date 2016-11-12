558b5aa83b88ac9be62518f9ae2717a6e06ffe78
tinouS <tinoudi.simohammed@gmail.com>
2016-11-12 12:08:51 +0100

    ..

--- web/SCRUMApp/public/angular/controller/userstories/list.js
+++ web/SCRUMApp/public/angular/controller/userstories/list.js
@@ -1,12 +1,10 @@
 angular.module('UserStories')
     .controller('UserStoriesListController', ['$scope', '$location', 'UserStoriesServices',
         function ($scope, $location, UserStoriesServices) {
-            //console.log($scope.params.project_id);
 
             if ($scope.params.project_id){
                 UserStoriesServices.get($scope.params.project_id).then(function(response){
                     $scope.listUserStories = response.data;
-
                     UserStoriesServices.setListUS($scope.listUserStories);
                 });
             }
